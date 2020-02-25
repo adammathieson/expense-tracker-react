7ecc758b13b18483c9513d462b27b08c67a43480
Adam Mathieson <mathieson1222@gmail.com>
2020-02-24 21:38:01 -0800

    adds state for form inputs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import {Header} from './components/Header'
 import {Balance} from './components/Balance'
 import {IncomeExpenses} from './components/IncomeExpenses'
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div class="container">
+      <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
